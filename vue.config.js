d23ad9786106db77755f5ef34a420a3b56bd8002
gamenwp <65244578+gamenwp@users.noreply.github.com>
2022-07-31 23:14:39 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    ? '/huyndaiWM/'
     : '/'
 })
